ca8a9b32219810c364d4a82bc16222e93a671a8a
jpelaa <jpelaa1991994@gmail.com>
2023-11-11 09:50:17 +0530

    checkout ui

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -59,11 +59,9 @@ export default function Home() {
                 className="bg-gray-50 border-2  py-3 border-gray-300  text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pl-12"
                 placeholder=""
               />
-              <div className="absolute top-2 right-0 flex items-center  pointer-events-none ">
-                <button className="text-white bg-purple rounded-full">
-                  apply
-                </button>
-              </div>
+              <button className="text-white bg-purple rounded-full absolute top-1.5 bottom-1.5 right-1.5 px-3 py-1 flex items-center">
+                apply
+              </button>
             </div>
           </div>
         </div>
